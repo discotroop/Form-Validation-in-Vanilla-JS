203bf6a85571199f5389d4d5bdd1e2e3be93aa4f
John Mark Goodson <john.mark.hat@gmail.com>
2019-11-18 15:02:43 -0500

    password has length requirement, confirmpassword now has to match, confirm email has to match.

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
-console.log("testing");
+function validateForm () {
+    const email = document.getElementById("email");
+    const confirmEmail = document.getElementById("confirmEmail");
+    const country = document.getElementById("country");
+    const phone = document.getElementById("phone");
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirmPassword");
+    const submit = document.getElementById("submit")
+
+    submit.addEventListener("click", function () {
+        console.log(password.value);
+        console.log(confirmPassword.value) 
+    })
+
+    email.addEventListener("input", function (event) {
+        if (email.validity.typeMismatch) {
+            email.setCustomValidity("Please enter a valid email address");
+        } else {
+            email.setCustomValidity("");
+        }
+    });
+
+    confirmEmail.addEventListener("input", function(event) {
+        if (email.value !== confirmEmail.value) {
+            confirmEmail.setCustomValidity("Email addresses must match")
+        } else {
+            confirmEmail.setCustomValidity("");
+        }
+        console.log(confirmEmail)
+    });
+
+    password.addEventListener("input", function (event)  {
+        if (password.validity.typeMismatch) {
+            password.setCustomValidity("Password must be atleast 6 characters and no more than 20")
+        } else {
+            password.setCustomValidity("");
+        }
+    })
+
+    confirmPassword.addEventListener("input", function(event) {
+        if (password.value !== confirmPassword.value) {
+            console.log("wrong");
+            confirmPassword.setCustomValidity("Passwords must match!")
+        } else {
+            confirmPassword.setCustomValidity("");
+        }
+    })
+}
+
+validateForm();
+
